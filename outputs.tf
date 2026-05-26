f2d29f88a43ca840aabe3081fb63785d4e542fc6
harry.sarshogh <harry.sarshogh@gmail.com>
2026-05-26 23:15:50 +0100

    feat: implement tf-atom-lb-target-group-aws module

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,24 @@
 output "enabled" {
-  description = "Whether the module is enabled."
+  description = "Whether the module is enabled"
   value       = local.enabled
+}
+
+output "id" {
+  description = "ID of the target group"
+  value       = try(aws_lb_target_group.this[0].id, null)
+}
+
+output "arn" {
+  description = "ARN of the target group"
+  value       = try(aws_lb_target_group.this[0].arn, null)
+}
+
+output "arn_suffix" {
+  description = "ARN suffix for CloudWatch metrics"
+  value       = try(aws_lb_target_group.this[0].arn_suffix, null)
+}
+
+output "name" {
+  description = "Name of the target group"
+  value       = try(aws_lb_target_group.this[0].name, null)
 }
